f121155ae6c193fb7e4350e9ee60c0c80e610c61
wanjugucode <karobiaanastasia.com>
2021-05-28 20:04:22 +0300

    code

--- basics.js
+++ basics.js
@@ -1,16 +1,16 @@
 let btn=document.getElementById(`btn`);
 let output=document.getElementById(`outputtext`);
-let number=[Math.floor(Math.random()*100)]
+let number=[Math.floor(Math.random()*10)]
 btn.addEventListener(`click`,function(){
 let input=document.getElementById(`userinput`).value;
 if(input==number){
     output.innerHTML=`You guessed right. your number was ${number}`
 }
 else if(input<number){
-    output.innerHTML=`you guessed too low!`
+    output.innerHTML=`Almost there.guess again!`
 }
 if (input>number){
-  output.innerHTML=`you guessed too high!`
+  output.innerHTML=`Guess again!`
 }
 })
  
